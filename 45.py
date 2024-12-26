954aab10695bcc6ed559b5446528e84575ab5694
kamoliddindev1710 <kamoliddinfazliddinov17@gmail.com>
2024-12-26 12:47:10 +0500

    Add files via upload

--- 45.py
+++ 45.py
@@ -2,25 +2,25 @@ def cash_game(l:list,t:list):
         a1=3
         a2=3
         for x in range(len(l)):
-            if l[x]==t[x] and l[x]=="share":
+            if l[x]==t[x] and l[x]=="share" and (a1>0 and a2>0):
                 a1=a1+2
                 a2=a2+2
-            elif l[x]==t[x] and l[x]=="steal":
+            elif l[x]==t[x] and l[x]=="steal" and (a1>0 and a2>0):
                 a1=a1+0
                 a2=a2+0
-            elif l[x]=="steal" and t[x]=="share":
+            elif l[x]=="steal" and t[x]=="share" and (a1>0 and a2>0):
                 a1=a1+3
                 a2=a2-1
-            elif l[x]=="share" and t[x]=="steal":
+            elif l[x]=="share" and t[x]=="steal" and (a1>0 and a2>0):
                 a1=a1-1
                 a2=a2+3
         print([a1,a2])
 
 try:
-    text=input("first opponent enter the words ")
-    text2=input("Second opponent enter the words: ")
+    text1=input("Enter your choices <> [share] or [steal]: ")
+    text2=input("Enter your choices <> [share] or [steal]: ")
 except Exception as ex:
      print(ex)
-fir_text=text.split(" ")
-sec_text=text2.split(" ")
-cash_game(fir_text,sec_text)
+fir_op=text1.split(" ")
+sec_op=text2.split(" ")
+cash_game(fir_op,sec_op)
